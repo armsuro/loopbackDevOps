8d9079adb3a4ba677456a6ff48722e07c00a855e
Suren Gasparyan <suren.gasparyan1997@gmail.com>
2017-02-08 16:01:25 +0400

    Pull scripts

--- server/boot/root.js
+++ server/boot/root.js
@@ -5,11 +5,49 @@ module.exports = function(server) {
     var router = server.loopback.Router();
     var shell = require('shelljs');
     router.get('/', server.loopback.status());
+    var homePath = "/home/vartan"
 
-    router.get('/ping', function(req, res) {
-        shell.exec("cd /var/www/html && mkdir testttt", {
+    var pullScript = function(progectName) {
+        shell.exec("cd " + homePath + " && bash pull.sh " + progectName, {
             silent: true,
             async: true
+        }, function(done) {
+            shell.exec("cd " + homePath + "/" + progectName + " && docker exec api /app/node_modules/.bin/pm2 restart all", {
+                silent: true,
+                async: true
+            }, function(done) {
+                return true
+            });
+        });
+    }
+
+    router.post('/pull', function(req, res) {
+        if (req.body.progect) {
+            pullScript(req.body.progect)
+            res.json({
+                success: true
+            })
+        } else {
+            res.json({
+                "success": false,
+                "message": "Parametrs Incorect"
+            })
+        }
+    });
+
+    router.post('/pullAll', function(req, res) {
+        shell.exec("cd " + homePath + " && bash pull.sh all", {
+            silent: true,
+            async: true
+        }, function(done) {
+            shell.exec("cd " + homePath + "/dataowl && docker exec api /app/node_modules/.bin/pm2 restart all && cd " + homePath + "/oxford && docker exec api /app/node_modules/.bin/pm2 restart all && cd " + homePath + "/envi && docker exec api /app/node_modules/.bin/pm2 restart all && cd " + homePath + "/thegood && docker exec api /app/node_modules/.bin/pm2 restart all", {
+                silent: true,
+                async: true
+            }, function(done) {
+                res.json({
+                    success: true
+                })
+            });
         });
     });
     server.use(router);
